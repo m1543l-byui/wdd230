66553be1e303a9a5223b9a7c1d48e9a086eae8d7
m1543l-byui <m1543l@byui.edu>
2023-01-06 13:34:52 -0700

    Started Portal Page

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let date = new Date();
+let year =date.getFullYear();
+
+document.querySelector('h1').innerHTML = '&copy;' + year;
+
+let currentdate = document.lastModified;
+
+document.querySelector('div').textContent = `Last Updated: currentdate`;
+
+console.log(year);
