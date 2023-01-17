b14af2dc72ee6c05e8a5ab37a997c3c4eceb4c49
m1543l-byui <m1543l@byui.edu>
2023-01-17 09:32:35 -0800

    Edited HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
+
 let date = new Date();
 let year =date.getFullYear();
 
-document.querySelector('h1').innerHTML = '&copy;' + year;
+document.getElementById('date').innerHTML = '&copy;' + year;
 
 let currentdate = document.lastModified;
 
-document.querySelector('div').textContent = `Last Updated: ${currentdate}`;
+document.getElementById('date').textContent +=  ` Last Updated: ${currentdate}`;
 
 console.log(year);
